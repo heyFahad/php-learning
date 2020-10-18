6b7f9fee2b1a08c5c86441f2dc8e941766610f4d
Fahad Javed <Fahad_Javed008@yahoo.com>
2020-10-18 19:10:28 +0500

    Step 7: Associative Arrays
    An quick introduction to associative arrays in Php; how we can create them, how to loop over them using foreach loop, how to add new items in an array, and also how to delete items from an array (using a Php construct 'unset')

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
 
-$names = [
-    'Jeff',
-    'John',
-    'Mary'
+$person = [
+    'age' => 31,
+    'hair' => 'brown',
+    'career' => 'web developer'
 ];
+
+$person['name'] = 'Jeffrey';
+
+unset($person['age']);
 
 require 'index.view.php';
--- index.view.php
+++ index.view.php
@@ -3,19 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
-    <style>
-        header {
-            background: #e3e3e3;
-            padding: 2em;
-            text-align: center;
-        }
-    </style>
 </head>
 <body>
-    
+
     <ul>
-        <?php foreach ($names as $name) : ?>
-            <li><?= $name; ?></li>
+        <?php foreach ($person as $key => $feature) : ?>
+            <li><strong><?= $key; ?></strong> <?= $feature; ?></li>
         <?php endforeach; ?>
     </ul>
 
